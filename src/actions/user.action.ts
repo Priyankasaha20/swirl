a41ed5a2625ee04b8c36c9aab075d77a106d7898
unknown <priyankamay42@gmail.com>
2025-01-03 22:24:58 +0530

    sidebar

---
+++ src/actions/user.action.ts
@@ -0,0 +1,14 @@
+// "use server";
+
+// import { currentUser } from "@clerk/nextjs/server";
+
+// export  async function syncUser() 
+// {
+//   try {
+//     const {userId}=await auth()
+//   const   user=await currentUser();
+//   if(!userId || !user) return;
+//   } catch (error) {
+    
+//   }  
+// }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,10 @@ import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
 
 
+
  function Navbar() {
-  
+  // const user=await currentUser();
+  // if(user) await syncUser();
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
